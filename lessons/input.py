99096de082fd3ea7ab1df774faccdf2b9bff39bd
Yawen Deng <yawend@ad.unc.edu>
2022-08-20 21:58:19 -0400

    "Finished LS06!"

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demostrates asking the user for input."""
+
+user_name: str = input ("What is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awsome, " + user_name)
