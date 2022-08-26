c27d5c9831d5d6cb2830755858816b67f03449ae
Yawen Deng <yawend@ad.unc.edu>
2022-08-26 17:29:55 -0400

    "Progress on Exercise 1"

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,14 +2,12 @@
 __author__ = "730607229"
 
 word: str = input("Enter a 5-character word: ")
-
 if len(word) != 5:
-    exit("Error: Word must contain 5 characters")
+    exit("Error: Word must contain 5 characters ")
 
 single_character: str = input("Enter a single character: ")
-
 if len(single_character) != 1:
-    exit("Error: Character must be a single character")
+    exit("Error: Character must be a single character.")
 
 matching_number: int = 0
 print("Searching for " + single_character + " in " + word)
@@ -32,5 +30,7 @@ if single_character == word[4]:
 
 if matching_number == 0:
     print("No instances of " + single_character + " found in " + word)
-else:
+if matching_number == 1:
+    print(str(matching_number) + " instance of " + single_character + " found in " + word)
+if matching_number > 1:
     print(str(matching_number) + " instances of " + single_character + " found in " + word)
