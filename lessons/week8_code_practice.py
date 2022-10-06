836584770e2e458f535268abc76d0419a6033334
Yawen Deng <yawend@ad.unc.edu>
2022-10-06 13:45:13 -0400

    "lessons"

---
+++ lessons/week8_code_practice.py
@@ -0,0 +1,10 @@
+"""Code Writing Practice."""
+
+
+def zip(ks: list[str], vs: list[str]) -> dict[str, str]:
+    """Build a dictionary from two lists."""
+    assert len(ks) == len(vs)
+    result: dict[str, str] = {}
+    for i in range(0, len(ks)):
+        result[ks[i]] = vs[i]
+    return result
