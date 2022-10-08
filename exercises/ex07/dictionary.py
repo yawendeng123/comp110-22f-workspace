d263e31b2ed5550e24e8ab85ba9b266c88486582
Yawen Deng <yawend@ad.unc.edu>
2022-10-07 21:46:44 -0400

    "Eventually finish ex07, now I'm a dead body."

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,54 @@
+"""EX07 - Dictionary Functions."""
+__author__ = "730607229"
+
+
+from turtle import color
+
+
+def invert(initial: dict[str, str]) -> dict[str, str]:
+    """Given a dictionary of [str, str], invert should return a dict[str, str] that inverts the keys and the values. The keys of the input list becomes the values of the output list and vice versa."""
+    result: dict[str, str] = {}
+    check: list[str] = []
+    for i in initial:
+        if initial[i] not in check:
+            check.append(initial[i])
+    if len(check) != len(initial):
+        raise KeyError("Keys in a dictionary should be unique")
+    for key in initial:
+        result[initial[key]] = key
+    return result
+
+
+def count(xs: list[str]) -> dict[str, int]:
+    """Given a list[str], produce a dict[str, int] where each key is a unique value in the given list and each value associated is the count of the number of times that value appeared in the input list."""
+    counter_dictionary: dict[str, int] = {}
+    for i in xs:
+        if i in counter_dictionary:
+            counter_dictionary[i] += 1
+        else:
+            counter_dictionary[i] = 1
+    return counter_dictionary
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Given a dictionary of [str, str] of names and favorite colors, return a str which is the color that appears most frequently. If there is a tie for most popular color, return the color that appeared in the dictionary first."""
+    counter: dict[str, int] = {}
+    value_list: list[str] = []
+    for key in colors:
+        value_list.append(colors[key])
+    for i in value_list:
+        if i in counter:
+            counter[i] += 1
+        else:
+            counter[i] = 1
+    key_list: list[str] = []
+    for key in counter:
+        key_list.append(key)
+    maximum: int = 0
+    popular: str = ""
+    for key in counter:
+        popular = key_list[0]
+        if counter[key] > maximum:
+            maximum = counter[key]
+            popular = key
+    return popular
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,97 @@
+"""EX07 - Dictionary Test Functions."""
+__author__ = "730607229"
+
+
+import pytest
+from dictionary import invert, favorite_color, count
+
+
+def test_invert_keyerror() -> None:
+    """Test funtion invert with duplicated values in initial dictionary, raise a KeyError."""
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
+
+def test_invert_empty_dictionary() -> None:
+    """Test function invert with an empty dictionary."""
+    dictionary: dict[str, str] = {}
+    assert invert(dictionary) == {}
+
+
+def test_invert_one_pair() -> None:
+    """Test function invert with one key-value pair."""
+    dictionary: dict[str, str] = {"one and only": "best friend"}
+    assert invert(dictionary) == {"best friend": "one and only"}
+
+
+def test_invert_multiple_pairs() -> None:
+    """Test function invert with multiple key-value pairs."""
+    dictionary: dict[str, str] = {"a": "z", "b": "y", "c": "x"}
+    assert invert(dictionary) == {"z": "a", "y": "b", "x": "c"}
+
+
+def test_invert_multiple_pairs_again() -> None:
+    """Test function invert with multiple key-value pairs again."""
+    dictionary: dict[str, str] = {"money": "monkey", "CocaCola": "Sprite", "Pepsi": "Mtn Dew", "dog": "cheems", "cat": "buluga"}
+    assert invert(dictionary) == {"monkey": "money", "Sprite": "CocaCola", "Mtn Dew": "Pepsi", "cheems": "dog", "buluga": "cat"}
+
+
+def test_count_list_empty() -> None:
+    """Test function count with empty list."""
+    xs: list[str] = []
+    assert count(xs) == {}
+
+
+def test_count_list_one_item_once() -> None:
+    """Test function count with only one item occurs once in the list."""
+    xs: list[str] = ["doge"]
+    assert count(xs) == {"doge": 1}
+
+
+def test_count_list_one_item_multiple_times() -> None:
+    """Test function count with only one item occurs multiple times in the list."""
+    xs: list[str] = ["love Suguru", "love Suguru", "love Suguru", "love Suguru", "love Suguru"]
+    assert count(xs) == {"love Suguru": 5}
+
+
+def test_count_list_multiple_item_once() -> None:
+    """Test function count with multiple items occur only once in the list."""
+    xs: list[str] = ["doge", "cheems", "buluga", "hecker"]
+    assert count(xs) == {"doge": 1, "cheems": 1, "buluga": 1, "hecker": 1}
+
+
+def test_count_list_multiple_item_multiple_times() -> None:
+    """Test function count with multiple items occur multiple times in the list."""
+    xs: list[str] = ["monkey", "money", "Satoru", "money", "monkey", "curse", "curse", "monkey", "money", "monkey", "monkey"]
+    assert count(xs) == {"monkey": 5, "Satoru": 1, "money": 3, "curse": 2}
+
+
+def test_favorite_color_empty() -> None:
+    """Test function favorite_color with empty colors dictionary."""
+    dictionary: dict[str, str] = {}
+    assert favorite_color(dictionary) == ""
+
+
+def test_favorite_color_one_item() -> None:
+    """Test function favorite_color with one item in dictionary."""
+    dictionary: dict[str, str] = {"Karmen": "blue"}
+    assert favorite_color(dictionary) == "blue"
+
+
+def test_favorite_color_multiple_item_same_color() -> None:
+    """Test function favorite_color with multiple people love the same color in dictionary."""
+    dictionary: dict[str, str] = {"Karmen": "blue", "Coco": "blue", "Kris": "blue"}
+    assert favorite_color(dictionary) == "blue"
+
+
+def test_favorite_color_multiple_item_multiple_color() -> None:
+    """Test function favorite_color with mutiple people love different colors in dictionary."""
+    dictionary: dict[str, str] = {"Karmen": "blue", "Evelina": "pink", "Hua": "white", "Coco": "blue", "Kris": "blue"}
+    assert favorite_color(dictionary) == "blue"
+
+
+def test_favorite_color_tie_colors() -> None:
+    """Test function favorite_color with a tie for most popular color in dictionary."""
+    dictionary: dict[str, str] = {"Karmen": "blue", "Evelina": "pink", "Lydia": "pink", "Hua": "white", "Coco": "blue"}
+    assert favorite_color(dictionary) == "blue"
