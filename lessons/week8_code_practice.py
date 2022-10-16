cd3d31867d8d2c0b621bdb93dea293c83751a2c1
Yawen Deng <yawend@ad.unc.edu>
2022-10-16 19:18:30 -0400

    "week 8"

--- lessons/week8_code_practice.py
+++ lessons/week8_code_practice.py
@@ -8,3 +8,9 @@ def zip(ks: list[str], vs: list[str]) -> dict[str, str]:
     for i in range(0, len(ks)):
         result[ks[i]] = vs[i]
     return result
+
+    # if we use while loop
+    # i: int = 0
+    # while i < len(ks):
+        # result[ks[i]] = vs[i]
+    # return result
